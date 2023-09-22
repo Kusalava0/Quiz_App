8e8d0773091cc521f5d35b4710a150f6ec18abf0
Kusalava0 <dkb03122001@gmail.com>
2023-09-23 02:13:07 +0530

    removed fonts

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
         <script src="https://kit.fontawesome.com/87250e21ec.js" crossorigin="anonymous"></script>
       </head>
       <body className={inter.className}>{children}</body>
